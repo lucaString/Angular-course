59c4170d6d7ceff9d0cbc4f4abff43b8eb237352
lucaString <lstringhetti@sandsmedia.com>
2023-06-29 15:26:09 +0200

    adding cards with string interpolation

--- src/app/prova/prova.component.ts
+++ src/app/prova/prova.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-prova',
   templateUrl: './prova.component.html',
-  styleUrls: ['./prova.component.css']
+  styleUrls: ['./prova.component.css'],
 })
-export class ProvaComponent implements OnInit {
+export class ProvaComponent
+  implements
+    OnInit,
+    AfterContentChecked,
+    AfterContentInit,
+    AfterViewChecked,
+    AfterViewInit,
+    DoCheck,
+    OnDestroy
+{
+  // cani = [
+  //   {
+  //     nome: 'Roger',
+  //     razza: 'Goldenretriver',
+  //     descrizione: `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog 
+  //     from Japan. A small, agile dog that copes very well withmountainous terrain, the Shiba Inu was 
+  //     originally bred for hunting.`,
+  //   }
+  // ];
 
-  constructor() { }
+  constructor() {
+    console.log('costruttore');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
 
   ngOnInit(): void {
-    
+    console.log('ngOnInit');
   }
 }
