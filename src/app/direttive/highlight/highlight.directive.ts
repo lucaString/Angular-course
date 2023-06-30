e6cb081b6425ac356c35cca3f585b51c0ee0ddca
lucaString <lstringhetti@sandsmedia.com>
2023-06-30 11:23:58 +0200

    create simple directive

--- src/app/direttive/highlight/highlight.directive.ts
+++ src/app/direttive/highlight/highlight.directive.ts
@@ -1,10 +1,24 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private element: ElementRef) {
+
+   }
+
+   @HostListener('mouseenter') onMouseEnter() {
+    this.cambiaColore('yellow')
+
+   }
+   @HostListener('mouseleave') onMouseleave() {
+    this.cambiaColore('red')
+   }
+
+   cambiaColore(colore: string) {
+    this.element.nativeElement.style.backgroundColor = colore
+   } 
 
 }
