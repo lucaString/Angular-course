1e22673561948cbae64eb70b0eff5a2370c69fdb
lucaString <lstringhetti@sandsmedia.com>
2023-06-30 10:02:56 +0200

    adding props drilling

--- src/app/prova/prova.component.ts
+++ src/app/prova/prova.component.ts
@@ -1,6 +1,7 @@
 import {
   Component,
-  OnInit
+  OnInit,
+  Input
 } from '@angular/core';
 
 @Component({
@@ -12,19 +13,20 @@ export class ProvaComponent
   implements
     OnInit
 {
-  immage1 = `https://images.unsplash.com/photo-1610575828349-43f195e95452?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1286&q=80`
-  immage2 = `https://material.angular.io/assets/img/examples/shiba2.jpg`
-  cani = [
-    {
-      nome: 'Roger',
-      razza: 'Goldenretriver',
-      descrizione: `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog 
-      from Japan. A small, agile dog that copes very well withmountainous terrain, the Shiba Inu was 
-      originally bred for hunting.`,
-    }
-  ];
+  // immage1 = `https://images.unsplash.com/photo-1610575828349-43f195e95452?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1286&q=80`
+  // immage2 = `https://material.angular.io/assets/img/examples/shiba2.jpg`
+  // isDisabled = false
+  // cani = [
+  //   {
+  //     nome: 'Roger',
+  //     razza: 'Goldenretriver',
+  //     descrizione: `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog 
+  //     from Japan. A small, agile dog that copes very well withmountainous terrain, the Shiba Inu was 
+  //     originally bred for hunting.`,
+  //   }
+  // ];
 
-  isDisabled = false
+  @Input() data: any;
 
   constructor() {
     console.log('costruttore');
@@ -32,7 +34,7 @@ export class ProvaComponent
   
 
   ngOnInit(): void {
-    console.log('ngOnInit');
+    console.log(this.data);
 
   }
 }
